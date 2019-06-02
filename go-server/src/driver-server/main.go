509e6cce94de182cc3549c8d734d266687179d8b
Luciano Almeida <passos.luciano@outlook.com>
2019-06-02 10:18:12 -0300

    Implementing simple list server

--- go-server/src/driver-server/main.go
+++ go-server/src/driver-server/main.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"log"
 	"net"
-	"google.golang.org/grpc"
+
 	pb "drivers.org"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -16,8 +17,33 @@ type server struct{}
 
 func (s *server) FindDriver(ctx context.Context, in *pb.DriverRequest) (*pb.DriverResponse, error) {
 	log.Printf("Received: %d", in.Number)
-
-	return nil, nil
+	drivers := map[int32]string{
+		44: "Lewis Hamilton",
+		77: "Valtteri Bottas",
+		5:  "Sebastian Vettel",
+		16: "Charles Leclerc",
+		10: "Pierre Gasly",
+		33: "Max Verstappen",
+		3:  "Daniel Ricciardo",
+		27: "Nico Hulkenberg",
+		8:  "Romain Grosjean",
+		20: "Kevin Magnussen",
+		55: "Carlos Sainz",
+		4:  "Lando Norris",
+		11: "Sergio Perez",
+		18: "Lance Stroll",
+		7:  "Kimi Raikkonen",
+		99: "Antonio Giovinazzi",
+		26: "Daniil Kvyat",
+		23: "Alexander Albon",
+		63: "George Russell",
+		88: "Robert Kubica",
+	}
+	name := drivers[in.Number]
+	if name == "" {
+		return nil, nil
+	}
+	return &pb.DriverResponse{Name: name}, nil
 }
 
 func main() {
---
+++ grpc-client/grpc-client/Service/DriverService.swift
@@ -0,0 +1,13 @@
+//
+//  DriverService.swift
+//  grpc-client
+//
+//  Created by Luciano Almeida on 01/06/19.
+//  Copyright © 2019 Luciano Almeida. All rights reserved.
+//
+
+import UIKit
+
+class DriverService: NSObject {
+
+}
