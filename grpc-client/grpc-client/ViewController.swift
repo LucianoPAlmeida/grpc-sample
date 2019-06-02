83dd59102d2e2db1e75a9bb40ac2cceff91883ff
Luciano Almeida <passos.luciano@outlook.com>
2019-06-01 21:18:50 -0300

    Setup iOS Client

---
+++ grpc-client/grpc-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  grpc-client
+//
+//  Created by Luciano Almeida on 01/06/19.
+//  Copyright © 2019 Luciano Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
